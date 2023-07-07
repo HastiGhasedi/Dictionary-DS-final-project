d133987bf8dabae01f0ade75f736fdffa1d0a48b
HastiGhasedi <dandelion.hgh@gmail.com>
2023-07-07 15:54:34 +0330

    My second commit

--- dictionary.py
+++ dictionary.py
@@ -7,10 +7,18 @@ def find_synonyms(word) :
     synonyms = dictionary.synonym(word)
     return synonyms
 
+def generate_example_sentence(word) :
+    example_sentence = " "
+    for syn in Wordnet.synsets(word):
+        for example in syn.examples():
+            example_sentence += example + " "
+    return example_sentence
+
 if __name__ == '__main__':
     word = input("Enter a word please: ")
 
     synonyms = find_synonyms(word)
+    example_sentence = generate_example_sentence(word)
 
-    display_results(word, synonyms)
+    display_results(word, synonyms, example_sentence)
     
