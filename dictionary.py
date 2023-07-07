62bde0cfc0e7766d5f13f73e05579e8560c1b8ab
HastiGhasedi <dandelion.hgh@gmail.com>
2023-07-07 16:00:15 +0330

    My third commit

--- dictionary.py
+++ dictionary.py
@@ -14,10 +14,22 @@ def generate_example_sentence(word) :
             example_sentence += example + " "
     return example_sentence
 
+def find_similar_word(word) :
+    similar_words = []
+    for syn in Wordnet.synsets(word) :
+        for lemma in syn.lemmas() :
+            similar_word = lemma.name()
+            if similar_word != word and similar_word not in similar_words :
+                similar_words.append(similar_word)
+            if len(similar_words) == 2:
+                return similar_words
+    return similar_words
+
 if __name__ == '__main__':
     word = input("Enter a word please: ")
 
     synonyms = find_synonyms(word)
+
     example_sentence = generate_example_sentence(word)
 
     display_results(word, synonyms, example_sentence)
